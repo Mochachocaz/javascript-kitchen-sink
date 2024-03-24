4f743726f43c61921df682ecace5f99ae1ac9d4a
Sam Martin <sammartin1357@gmail.com>
2024-03-24 18:09:16 +0800

    adding first three states of lab

---
+++ app.js
@@ -0,0 +1,5 @@
+let myName = "Sam" // making a variable called myName and defining it as Sam
+
+const USStates = 51 // making a constant variable called USStates and defining it as 51
+
+var result = (5 + 4) // making a variable thats the result of adding 5 and 4
