ccf2f15ef5a2d5d6d1602c683a0c13b2e167e537
Sam Martin <sammartin1357@gmail.com>
2024-03-24 18:19:16 +0800

    added functioning sayHello function

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
-let myName = "Sam" // making a variable called myName and defining it as Sam
+let myName = "Sam"; // making a variable called myName and defining it as Sam
 
-const USStates = 51 // making a constant variable called USStates and defining it as 51
+const USStates = 51; // making a constant variable called USStates and defining it as 51
 
-var result = (5 + 4) // making a variable thats the result of adding 5 and 4
+var result = (5 + 4); // making a variable thats the result of adding 5 and 4
+
+function sayHello() {
+    alert('Hello World!')
+};
+sayHello();
